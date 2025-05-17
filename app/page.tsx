c92cbd916f705ee54849d82658d25c492c1242c3
David Ernst <git@dsernst.com>
2025-05-17 02:24:44 -0700

    Remove cartoon robot face

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,11 @@
-import Image from 'next/image'
 import Features from './components/Features'
-import RobitImg from './components/robit_head.png'
 
 export default function Home() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center gap-16 py-12">
       <div className="text-center">
         <h1 className="text-6xl font-bold mb-6 flex justify-center items-center gap-5">
-          <Image src={RobitImg} alt="Robit" width={70} /> Robit
+          Robit
         </h1>
         <p className="text-2xl">
           Programmable buddy for <span className="inline-block">💬</span> Signal
